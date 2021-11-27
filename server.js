5aec980626f51c21797a1ede7cea3667bb51a400
Singh1106 <Jaswindersingh11061998@gmail.com>
2021-11-28 00:57:16 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const app=new express();
+
+app.get('/',(req,res)=>{
+    res.send('Mern dev connector home page.');
+})
+
+const port=process.env.PORT || 4000;
+
+app.listen(port,()=>{
+    console.log(`Listening at port: ${port}`);
+})
