560032ca1f9eebed6427080f05489d7519bd4eff
Singh1106 <Jaswindersingh11061998@gmail.com>
2021-12-26 17:08:26 +0530

    Prepared for heroku Production

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config({ path: './config/dev.env' });
 const cors = require('cors');
 const express = require('express');
 const connectDB = require('./db/connectDB.js');
+const path = require('path');
 
 const usersRoute = require('./routes/api/userRoute');
 const postsRoute = require('./routes/api/postRoute');
@@ -23,14 +24,24 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.send('Mern dev connector home page.');
-});
+// not suited for production
+// app.get('/', (req, res) => {
+//   res.send('Mern dev connector home page.');
+// });
 
 // defining api routes
 app.use('/api/userRoute', usersRoute);
 app.use('/api/profileRoute', profileRoute);
 app.use('/api/postRoute', postsRoute);
+
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT;
 
